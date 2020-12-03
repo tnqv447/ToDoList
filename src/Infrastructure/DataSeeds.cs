6b643d0ec9800ebbb6d63d58512e7925f4ed2d61
hieukut456 <trunghieu9946@gmail.com>
2020-12-03 17:28:45 +0700

    update task detail gui

--- src/Infrastructure/DataSeeds.cs
+++ src/Infrastructure/DataSeeds.cs
@@ -9,7 +9,7 @@ namespace Infrastructure
         {
             context.Database.EnsureCreated();
 
-            if(!context.Users.Any())
+            if (!context.Users.Any())
             {
                 context.Users.AddRange(
                     new User("Nguyễn Văn A", "0123456789", "sdadad", ROLE.WORKER, USER_STATUS.ACTIVE, "nv1", "12345"),
@@ -22,41 +22,43 @@ namespace Infrastructure
                 );
                 context.SaveChanges();
             }
-            if(!context.ToDoTasks.Any()){
+            if (!context.ToDoTasks.Any())
+            {
                 context.ToDoTasks.AddRange(
                     new ToDoTask(
                         "Công việc 1",
                         "asdasdasd",
-                        DateTime.ParseExact("01/01/2020", "dd/MM/yyyy",System.Globalization.CultureInfo.InvariantCulture),
-                        DateTime.ParseExact("12/01/2020", "dd/MM/yyyy",System.Globalization.CultureInfo.InvariantCulture),
+                        DateTime.ParseExact("01/01/2020", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
+                        DateTime.ParseExact("12/01/2020", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
                         1,
                         STATUS.ON_PROGRESS,
                         SCOPE.PUBLIC),
                     new ToDoTask(
                         "Công việc 2",
                         "asdasdasd",
-                        DateTime.ParseExact("01/01/2020", "dd/MM/yyyy",System.Globalization.CultureInfo.InvariantCulture),
-                        DateTime.ParseExact("13/01/2020", "dd/MM/yyyy",System.Globalization.CultureInfo.InvariantCulture),
+                        DateTime.ParseExact("01/01/2020", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
+                        DateTime.ParseExact("13/01/2020", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
                         2,
                         STATUS.ON_PROGRESS,
                         SCOPE.PUBLIC),
                     new ToDoTask(
                         "Công việc 3",
                         "asdasdasd",
-                        DateTime.ParseExact("01/01/2020", "dd/MM/yyyy",System.Globalization.CultureInfo.InvariantCulture),
-                        DateTime.ParseExact("14/01/2020", "dd/MM/yyyy",System.Globalization.CultureInfo.InvariantCulture),
+                        DateTime.ParseExact("01/01/2020", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
+                        DateTime.ParseExact("14/01/2020", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
                         3,
                         STATUS.NEW,
                         SCOPE.PUBLIC),
                     new ToDoTask(
                         "Công việc 4",
                         "asdasdasd",
-                        DateTime.ParseExact("01/01/2020", "dd/MM/yyyy",System.Globalization.CultureInfo.InvariantCulture),
-                        DateTime.ParseExact("15/01/2020", "dd/MM/yyyy",System.Globalization.CultureInfo.InvariantCulture),
+                        DateTime.ParseExact("01/01/2020", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
+                        DateTime.ParseExact("15/01/2020", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
                         1,
                         STATUS.NEW,
                         SCOPE.PUBLIC)
                 );
+                // if(!context.)
                 context.SaveChanges();
             }
         }
